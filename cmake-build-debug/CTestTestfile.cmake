a4465bb49e5cef6efba4f3dd9733c47d15546b40
qxg <2569729045@qq.com>
2026-01-07 14:53:02 +0800

    123

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,14 @@
+# CMake generated Testfile for 
+# Source directory: /Users/xingeqi/CLionProjects/leveldb
+# Build directory: /Users/xingeqi/CLionProjects/leveldb/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(leveldb_tests "/Users/xingeqi/CLionProjects/leveldb/cmake-build-debug/leveldb_tests")
+set_tests_properties(leveldb_tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/xingeqi/CLionProjects/leveldb/CMakeLists.txt;367;add_test;/Users/xingeqi/CLionProjects/leveldb/CMakeLists.txt;0;")
+add_test(c_test "/Users/xingeqi/CLionProjects/leveldb/cmake-build-debug/c_test")
+set_tests_properties(c_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/xingeqi/CLionProjects/leveldb/CMakeLists.txt;393;add_test;/Users/xingeqi/CLionProjects/leveldb/CMakeLists.txt;396;leveldb_test;/Users/xingeqi/CLionProjects/leveldb/CMakeLists.txt;0;")
+add_test(env_posix_test "/Users/xingeqi/CLionProjects/leveldb/cmake-build-debug/env_posix_test")
+set_tests_properties(env_posix_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/xingeqi/CLionProjects/leveldb/CMakeLists.txt;393;add_test;/Users/xingeqi/CLionProjects/leveldb/CMakeLists.txt;404;leveldb_test;/Users/xingeqi/CLionProjects/leveldb/CMakeLists.txt;0;")
+subdirs("third_party/googletest")
+subdirs("third_party/benchmark")
---
+++ cmake-build-debug/third_party/benchmark/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/xingeqi/CLionProjects/leveldb/third_party/benchmark/src
+# Build directory: /Users/xingeqi/CLionProjects/leveldb/cmake-build-debug/third_party/benchmark/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
